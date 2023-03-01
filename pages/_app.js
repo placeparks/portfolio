83bf1732086a8d2f09c0309f4a5d6d5d1706d14e
placeparks <86215469+placeparks@users.noreply.github.com>
2023-03-02 02:30:48 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
 useEffect(()=>{
         import("bootstrap/dist/js/bootstrap");
 },[])
-  return <Component {...pageProps} />
+  return (
+<>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<Component {...pageProps} />
+</>
+);
 }
 
 export default MyApp
