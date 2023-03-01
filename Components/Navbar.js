5964010d0fa2e7a3d51ce7381324005966e1e2e5
placeparks <86215469+placeparks@users.noreply.github.com>
2023-03-02 02:50:42 +0500

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Navbar (props) {
+export default function Navbar () {
    return (
       <div>
     
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <a className="navbar-brand" href="#">Mirac.eth</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
